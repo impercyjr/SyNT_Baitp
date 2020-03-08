2e775278a7e7054bff9c5b64310d26b511db54f3
NGuyen Trung Sy <syntth1808031@fpt.edu.vn>
2020-03-08 17:04:36 +0700

    first commit

---
+++ HelloCovi/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace HelloCovi.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<HelloCovi.Models.MyDb>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(HelloCovi.Models.MyDb context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ HelloCovi/Models/MyDb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HelloCovi.Models
+{
+    public class MyDb : DbContext
+    {
+        public MyDb() : base("MyConnectionString")
+        {
+        }
+
+        public System.Data.Entity.DbSet<HelloCovi.Models.Student> Students { get; set; }
+    }
+}
